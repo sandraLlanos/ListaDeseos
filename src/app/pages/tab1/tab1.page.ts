fd64ff10930c7807e464a76d8461c38651592963
Sandra Llanos <sllanos@bancolombia.com.co>
2020-04-24 19:33:27 -0500

    services, routes and styles

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DeseosService } from 'src/app/services/deseos.service';
+import { Lista } from 'src/app/models/lista.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
-  constructor() {}
-
+  listas: Lista[]= []
+  constructor( private deseosService: DeseosService,
+               private router:Router  ) { }
+agregarlista(){
+  this.router.navigateByUrl('/agregar');
+}
 }
--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DeseosService } from 'src/app/services/deseos.service';
+import { Lista } from 'src/app/models/lista.model';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,6 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor( private deseosService: DeseosService ) {}
 
 }
