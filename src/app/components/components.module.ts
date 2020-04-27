8609a8125c1749a6b159bf04125f7e652734a586
Sandra Llanos <sllanos@bancolombia.com.co>
2020-04-27 11:17:00 -0500

    component module

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListasComponent } from './listas/listas.component';
+
+
+
+@NgModule({
+  declarations: [ListasComponent],
+  exports:[ListasComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentsModule { }
